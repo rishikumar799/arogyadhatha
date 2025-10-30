a7f58af645beb0eec8968d7e6648fc43810ae49d
rishikumarvadada <rishikumarvadada@gmail.com>
2025-10-30 10:52:47 +0000

    b j k

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   // experimental: {
   //   // Add the following line to enable the Node.js runtime for middleware
   //   unstable_allowDynamic: ['**/node_modules/google-logging-utils/**'],
